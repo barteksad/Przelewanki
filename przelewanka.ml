55b077e97db23d59751dbadbaed29ab86cd9429e
barteksad <barteksadlej@gmail.com>
2021-01-13 15:04:09 +0100

    testowanie

--- przelewanka.ml
+++ przelewanka.ml
@@ -18,7 +18,7 @@ let przelewanka a =
     let docelowe_objetosci =  Array.map snd a in
 
     let n = Array.length a in
-    let sprawdzone = Hashtbl.create (10000 * n) in
+    let sprawdzone = Hashtbl.create (100 * n) in
     let do_sprawdzenia = Queue.create () in
 
     Queue.add ((Array.make n 0),0) do_sprawdzenia;
@@ -26,45 +26,52 @@ let przelewanka a =
 
     while not (Queue.is_empty do_sprawdzenia) do
         let (aktualny,odleglosc) = Queue.take do_sprawdzenia in
-
+        let nowy = Array.copy aktualny in
+        
         for nr_szklanki = 0 to n-1 do
         (* dolewanie do pełna *)
-            let nowy = Array.copy aktualny in
             nowy.(nr_szklanki) <- objetosci.(nr_szklanki);
             if nowy != aktualny then
             begin
                 if nowy = docelowe_objetosci then raise (Wynik (odleglosc+1)) else
 
                 if Hashtbl.find_opt sprawdzone nowy = None then
+                (
                     Hashtbl.replace sprawdzone nowy true;
                     Queue.add (nowy,odleglosc+1) do_sprawdzenia;
+                )
             end;
         (*### *)
         (* wylewanie całości *)
-            let nowy = Array.copy aktualny in
             nowy.(nr_szklanki) <- 0;
             if nowy != aktualny then
             begin
                 if nowy = docelowe_objetosci then raise (Wynik (odleglosc+1)) else
 
                 if Hashtbl.find_opt sprawdzone nowy = None then
+                (
                     Hashtbl.replace sprawdzone nowy true;
                     Queue.add (nowy,odleglosc+1) do_sprawdzenia;
+                )
             end;
         (* ### *)
         (* przelewanie do innej *)
             for i = 0 to n-1 do
-                let roznica  = min (objetosci.(i) - aktualny.(i)) aktualny.(nr_szklanki) in
-                let nowy = Array.copy aktualny in
-                nowy.(i) <- objetosci.(i);
-                nowy.(nr_szklanki) <- nowy.(nr_szklanki) - roznica;
-                if nowy != aktualny then
+                if i != nr_szklanki then
                 begin
-                    if nowy = docelowe_objetosci then raise (Wynik (odleglosc+1)) else
-
-                    if Hashtbl.find_opt sprawdzone nowy = None then
-                        Hashtbl.replace sprawdzone nowy true;
-                        Queue.add (nowy,odleglosc+1) do_sprawdzenia;
+                    let roznica  = min (objetosci.(i) - aktualny.(i)) aktualny.(nr_szklanki) in
+                    nowy.(nr_szklanki) <- nowy.(nr_szklanki) - roznica;
+                    nowy.(i) <- nowy.(i) + roznica;
+                    if nowy != aktualny then
+                    begin
+                        if nowy = docelowe_objetosci then raise (Wynik (odleglosc+1)) else
+    
+                        if Hashtbl.find_opt sprawdzone nowy = None then
+                        (
+                            Hashtbl.replace sprawdzone nowy true;
+                            Queue.add (nowy,odleglosc+1) do_sprawdzenia;
+                        )
+                    end;
                 end;
             done;
         done;
